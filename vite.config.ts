a5406f0eb805cc9394105e904003a48cfe114057
Helgard Ferreira <hrfer3@gmail.com>
2023-03-26 23:31:36 +0200

    chore: final build configuration changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
+import dts from 'vite-plugin-dts';
+import path, { resolve } from 'path';
+import { existsSync, readdirSync, lstatSync, rmdirSync, unlinkSync } from 'fs';
+
 import machineFullReload from './vite-machine-full-reload';
+
+emptyDir(resolve(__dirname, 'dist'));
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,11 +14,8 @@ export default defineConfig({
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
       name: 'Globe',
-      formats: ['es', 'cjs'],
-      fileName: (format) => {
-        if (format === 'es') return 'index.js';
-        return `index.${format}.js`;
-      },
+      formats: ['es'],
+      fileName: 'index',
     },
     rollupOptions: {
       external: [
@@ -38,5 +40,32 @@ export default defineConfig({
     // Leave minification up to applications.
     minify: false,
   },
-  plugins: [react(), machineFullReload(['**/*.machine.ts', '**/machine.ts'])],
+  plugins: [
+    react(),
+    machineFullReload(['**/*.machine.ts', '**/machine.ts']),
+    dts({
+      skipDiagnostics: false,
+      rollupTypes: true,
+      staticImport: true,
+      insertTypesEntry: true,
+    }),
+  ],
 });
+
+function emptyDir(dir: string): void {
+  if (!existsSync(dir)) {
+    return;
+  }
+
+  for (const file of readdirSync(dir)) {
+    const abs = resolve(dir, file);
+
+    // baseline is Node 12 so can't use rmSync
+    if (lstatSync(abs).isDirectory()) {
+      emptyDir(abs);
+      rmdirSync(abs);
+    } else {
+      unlinkSync(abs);
+    }
+  }
+}
