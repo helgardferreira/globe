4ddae2185f6982fc11b976d2ba6b1d7ac33ff0c0
Helgard Ferreira <hrfer3@gmail.com>
2023-03-26 12:27:42 +0200

    feat: configure for project as npm package

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,36 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 import machineFullReload from './vite-machine-full-reload';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'Globe',
+      formats: ['es', 'cjs'],
+      fileName: (format) => `globe.${format}.js`,
+    },
+    rollupOptions: {
+      external: [
+        'react',
+        'react-dom',
+        'react-three-fiber',
+        'three',
+        'xstate',
+        '@xstate/react',
+        'rxjs',
+      ],
+      output: {
+        sourcemapExcludeSources: true,
+      },
+    },
+    sourcemap: true,
+    // Reduce bloat from legacy polyfills.
+    target: 'esnext',
+    // Leave minification up to applications.
+    minify: false,
+  },
   plugins: [react(), machineFullReload(['**/*.machine.ts', '**/machine.ts'])],
 });
